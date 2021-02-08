4caecd5bb2b7160770026f39d6607a265ffdb9d7
Tarikul islam <tarikul.cse5.bu@gmail.com>
2021-02-08 20:46:52 +0600

    unpkg scroll Reveal animation project

---
+++ app.js
@@ -0,0 +1,29 @@
+window.sr=ScrollReveal();
+sr.reveal('.animation-left',{
+	origin:'left',
+ 	duration:1000,
+ 	distance:'25rem',
+ 	delay:300
+
+});
+sr.reveal('.animation-right',{
+	origin:'right',
+ 	duration:1000,
+ 	distance:'25rem',
+ 	delay:300
+
+});
+sr.reveal('.animation-top',{
+	origin:'top',
+ 	duration:1000,
+ 	distance:'25rem',
+ 	delay:300
+
+});
+sr.reveal('.animation-bottom',{
+	origin:'bottom',
+ 	duration:1000,
+ 	distance:'25rem',
+ 	delay:300
+
+});
